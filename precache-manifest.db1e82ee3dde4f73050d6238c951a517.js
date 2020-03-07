b6e8365738108ab5783f37e859ae437b07493b9b
tammasorn <tammasorn_b@hotmail.com>
2020-03-07 16:52:36 +0700

    Updates

---
+++ precache-manifest.db1e82ee3dde4f73050d6238c951a517.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b703161dab681455ef4fb2c06af086",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dff5d538444a98591105",
+    "url": "/monsters-rolodex/static/css/main.835fe440.chunk.css"
+  },
+  {
+    "revision": "aa3c85476dd53b921f9f",
+    "url": "/monsters-rolodex/static/js/2.4f36d905.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.4f36d905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff5d538444a98591105",
+    "url": "/monsters-rolodex/static/js/main.4624af48.chunk.js"
+  },
+  {
+    "revision": "22162cf921b489951229",
+    "url": "/monsters-rolodex/static/js/runtime-main.1540e19d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
